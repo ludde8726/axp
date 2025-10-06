83889a8cdfb54eaa51192da97879bb05dca91e60
ludde8726 <ludvig.norling05@gmail.com>
2025-10-06 21:03:33 +0200

    Addition and subtraction of integers

--- cxp.c
+++ cxp.c
@@ -6,12 +6,12 @@
 #include "cxp.h"
 
 
-bool cxp_initi(CXP_Ctx *ctx, CXP_Int *x, uint32_t initial_capacity)
+bool cxp_initi(CXP_Ctx *ctx, CXP_Int *x, cxp_size_t initial_capacity)
 {
-    x->size = 0;
+    x->size = 1;
     x->capacity = initial_capacity;
     x->digits = calloc(initial_capacity, sizeof(cxp_digit_t));
-    x->sign = 1;
+    x->sign = 0;
     if (!x->digits) {
         cxp_throw(ctx, CXP_ERR_ALLOC, "Memory allocation failed, could not allocate %lu bytes.", initial_capacity*sizeof(cxp_digit_t));
         return false;
@@ -25,12 +25,12 @@ bool cxp_initf(CXP_Ctx *ctx, CXP_Float *x)
     return cxp_initf_ex(ctx, x, ctx->precision);
 }
 
-bool cxp_initf_ex(CXP_Ctx *ctx, CXP_Float *x, uint32_t precision)
+bool cxp_initf_ex(CXP_Ctx *ctx, CXP_Float *x, cxp_size_t precision)
 {
     x->size = 0;
     x->capacity = precision;
     x->digits = calloc(precision, sizeof(cxp_digit_t));
-    x->sign = 1;
+    x->sign = 0;
     x->exponent = 0;
     if (!x->digits) {
         cxp_throw(ctx, CXP_ERR_ALLOC, "Memory allocation failed, could not allocate %lu bytes.", precision*sizeof(cxp_digit_t));
@@ -38,6 +38,18 @@ bool cxp_initf_ex(CXP_Ctx *ctx, CXP_Float *x, uint32_t precision)
     }
     cxp_error_reset(ctx);
     return true;
+}
+
+void cxp_freei(CXP_Int *x)
+{
+    CXP_ASSERT(x->digits);
+    free(x->digits);
+}
+
+void cxp_freef(CXP_Float *x)
+{
+    CXP_ASSERT(x->digits);
+    free(x->digits);
 }
 
 bool cxp_copyi(CXP_Ctx *ctx, CXP_Int *restrict dst, const CXP_Int *restrict src)
@@ -55,7 +67,7 @@ bool cxp_copyf(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict 
     return cxp_copyf_ex(ctx, dst, src, ctx->precision); // We do not need to reset error here since init already does and nothing we do after can raise any errors
 }
 
-bool cxp_copyf_ex(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict src, uint32_t precision)
+bool cxp_copyf_ex(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict src, cxp_size_t precision)
 {
     CXP_ASSERT(!dst->digits);
     if (!cxp_initf_ex(ctx, dst, precision)) return false; // Sets the capacity for us
@@ -82,6 +94,129 @@ bool cxp_copyf_exact(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *res
     return true; // We do not need to reset error here since init already does and nothing we do after can raise any errors
 }
 
+int cxp_abs_cmpi(CXP_Ctx *ctx, const CXP_Int *x, const CXP_Int *y)
+{
+    // Maybe don't pass ctx to this function, however i like the consistancy.
+    (void)ctx;
+    if (x->size > y->size) return 1;
+    if (x->size < y->size) return -1;
+    for (cxp_size_t i = x->size; i > 0; i--) {
+        if (x->digits[i-1] > y->digits[i-1]) return 1;
+        if (x->digits[i-1] < y->digits[i-1]) return -1;
+    }
+    return 0;
+}
+
+cxp_size_t cxp__add_digits(const cxp_digit_t *x_digits, cxp_size_t x_sz, const cxp_digit_t *y_digits, cxp_size_t y_sz, cxp_digit_t *res)
+{
+    cxp_digit_t carry = 0;
+    cxp_size_t res_sz = 0;
+    cxp_size_t max_sz = (x_sz > y_sz) ? x_sz : y_sz;
+    for (cxp_size_t i = 0; (i < max_sz) || carry; i++) {
+        cxp_digit_t sum = carry;
+        if (i < x_sz) sum += x_digits[i];
+        if (i < y_sz) sum += y_digits[i];
+        res[i] = sum % 10;
+        carry = sum / 10;
+        res_sz++;
+    }
+    return res_sz;
+}
+
+bool cxp_addi(CXP_Ctx *ctx, const CXP_Int *x, const CXP_Int *y, CXP_Int *res)
+{
+    cxp_size_t max_sz = (x->size > y->size) ? x->size + 1 : y->size + 1;
+    if (!cxp_initi(ctx, res, max_sz)) return false;
+    if (x->sign != y->sign) {
+        if (x->sign) {
+            // This is quite repetetive, maybe refactor...
+            const CXP_Int *larger = y;
+            const CXP_Int *smaller = x;
+            cxp_digit_t res_sign = 0;
+            int cmp = cxp_abs_cmpi(ctx, y, x);
+            if (cmp == 0) return true;
+            else if (cmp < 0) {
+                res_sign = 1;
+                larger = x;
+                smaller = y;
+            }
+            cxp_size_t res_sz = cxp__sub_digits(larger->digits, larger->size, smaller->digits, smaller->size, res->digits);
+            res->size = res_sz;
+            res->sign = res_sign;
+            return true;
+        } else {
+            const CXP_Int *larger = x;
+            const CXP_Int *smaller = y;
+            cxp_digit_t res_sign = 0;
+            int cmp = cxp_abs_cmpi(ctx, x, y);
+            if (cmp == 0) return true;
+            else if (cmp < 0) {
+                res_sign = 1;
+                larger = x;
+                smaller = y;
+            }
+            cxp_size_t res_sz = cxp__sub_digits(larger->digits, larger->size, smaller->digits, smaller->size, res->digits);
+            res->size = res_sz;
+            res->sign = res_sign;
+            return true;
+        }
+    }
+    
+    
+    cxp_size_t res_sz = cxp__add_digits(x->digits, x->size, y->digits, y->size, res->digits);
+    res->size = res_sz;
+    res->sign = x->sign;
+    return true;
+}
+
+cxp_size_t cxp__sub_digits(const cxp_digit_t *x_digits, cxp_size_t x_sz, const cxp_digit_t *y_digits, cxp_size_t y_sz, cxp_digit_t *res)
+{
+    cxp_digit_t borrow = 0;
+    cxp_size_t res_sz = x_sz;
+    for (cxp_size_t i = 0; i < x_sz; i++) {
+        int diff = x_digits[i] - borrow;
+        if (i < y_sz) diff -= y_digits[i];
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else borrow = 0;
+        CXP_ASSERT(diff >= 0 && diff < 10);
+        res[i] = (cxp_digit_t) diff;
+    }
+    if (res[res_sz-1] == 0) return res_sz - 1;
+    return res_sz;
+}
+
+bool cxp_subi(CXP_Ctx *ctx, const CXP_Int *x, const CXP_Int *y, CXP_Int *res)
+{
+    cxp_size_t max_sz = (x->size > y->size) ? x->size : y->size;
+    if (!cxp_initi(ctx, res, max_sz)) return false;
+
+    if (x->sign != y->sign) {
+        // either (-x) - y or x - (-y) -> x + y 
+        //      = -(x + y) or x + y
+        cxp_size_t res_sz = cxp__add_digits(x->digits, x->size, y->digits, y->size, res->digits);
+        res->size = res_sz;
+        if (x->sign) res->sign = 1; // is negative
+        return true;
+    }
+    const CXP_Int *larger = x;
+    const CXP_Int *smaller = y;
+    cxp_digit_t res_sign = x->sign;
+    int cmp = cxp_abs_cmpi(ctx, x, y);
+    if (cmp == 0) {
+        return true;
+    } else if (cmp < 0) {
+        smaller = x;
+        larger = y;
+        res_sign = 1 - res_sign;
+    }
+    cxp_size_t res_sz = cxp__sub_digits(larger->digits, larger->size, smaller->digits, smaller->size, res->digits);
+    res->size = res_sz;
+    res->sign = res_sign;
+    return false;
+}
+
 void cxp_throw(CXP_Ctx *ctx, CXP_ErrorCode err_code, const char *fmt, ...) {
     ctx->err = err_code;
     va_list args;
@@ -99,7 +234,7 @@ static const char *cxp_error_messages[] = {
 };
 
 
-const char *cxp_error_str(const CXP_Ctx *ctx)
+const char *cxp_strerror(const CXP_Ctx *ctx)
 {
     if (*(ctx->err_str)) return ctx->err_str;
     return cxp_error_messages[ctx->err];
--- cxp.h
+++ cxp.h
@@ -50,33 +50,57 @@ typedef struct {
     int64_t exponent;
 } CXP_Float;
 
-bool cxp_initi(CXP_Ctx *ctx, CXP_Int *x, uint32_t initial_capacity);
+/* -- INIT FUNCTIONS -- */
+bool cxp_initi(CXP_Ctx *ctx, CXP_Int *x, cxp_size_t initial_capacity);
 bool cxp_initf(CXP_Ctx *ctx, CXP_Float *x);
-bool cxp_initf_ex(CXP_Ctx *ctx, CXP_Float *x, uint32_t precision);
+bool cxp_initf_ex(CXP_Ctx *ctx, CXP_Float *x, cxp_size_t precision);
+
+void cxp_freei(CXP_Int *x);
+void cxp_freef(CXP_Float *x);
+
+/* -- COPY FUNCTIONS -- */
 
 // Creates a `CXP_Int` copy of `src` into `dst` where `dst` is an unitialized `CXP_Int`
 // WARNING: Make sure that `src` and `dst` do NOT overlap!
+// Return: true on sucess and false on faliure and sets ctx->err accordingly
 bool cxp_copyi(CXP_Ctx *ctx, CXP_Int *restrict dst, const CXP_Int *restrict src);
 
 // Creates a copy of `src` into `dst` where `dst` is an unitialized `CXP_Float`
 // Note: This function will use the precision from the context, which may result in loss of precision.
 // For an exact copy use `cxp_copyf_exact` or `cxp_copyf_ex` for copying to a specified precision.
 // WARNING: Make sure that `src` and `dst` do NOT overlap!
+// Return: true on sucess and false on faliure and sets ctx->err accordingly
 bool cxp_copyf(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict src);
 // Creates a copy of `src` into `dst` where `dst` is an unitialized `CXP_Float`
 // Note: This function will use the precision from the supplied variable, if this variable is less
 // than the size of `src` this will result in a loss of precision.
 // For an exact copy use `cxp_copyf_exact`
 // WARNING: Make sure that `src` and `dst` do NOT overlap!
-bool cxp_copyf_ex(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict src, uint32_t precision);
+// Return: true on sucess and false on faliure and sets ctx->err accordingly
+bool cxp_copyf_ex(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict src, cxp_size_t precision);
 // Creates a copy of `src` into `dst` where `dst` is an unitialized `CXP_Float`
 // Note: This function will create an exact copy of `src` and will not respect the currently set
 // precision in the context.
 // WARNING: Make sure that `src` and `dst` do NOT overlap!
+// Return: true on sucess and false on faliure and sets ctx->err accordingly
 bool cxp_copyf_exact(CXP_Ctx *ctx, CXP_Float *restrict dst, const CXP_Float *restrict src);
 
+/* COMPARISON */
+// Compares the absolute value of two `Cxp_Int`
+// Return: `1` if `x > y`, `0`if `x == y` and `-1` if `x < y`
+int cxp_abs_cmpi(CXP_Ctx *ctx, const CXP_Int *x, const CXP_Int *y);
+
+/* ADD FUNCTIONS */
+// Returns the size of `res`
+cxp_size_t cxp__add_digits(const cxp_digit_t *x_digits, cxp_size_t x_sz, const cxp_digit_t *y_digits, cxp_size_t y_sz, cxp_digit_t *res);
+bool cxp_addi(CXP_Ctx *ctx, const CXP_Int *x, const CXP_Int *y, CXP_Int *res);
+
+// NOTE: Assumes that `x > y`
+cxp_size_t cxp__sub_digits(const cxp_digit_t *x_digits, cxp_size_t x_sz, const cxp_digit_t *y_digits, cxp_size_t y_sz, cxp_digit_t *res);
+bool cxp_subi(CXP_Ctx *ctx, const CXP_Int *x, const CXP_Int *y, CXP_Int *res);
+
 void cxp_throw(CXP_Ctx *ctx, CXP_ErrorCode err_code, const char *fmt, ...) PRINTF_LIKE_WARNINGS(3, 4);
-const char *cxp_error_str(const CXP_Ctx *ctx);
+const char *cxp_strerror(const CXP_Ctx *ctx);
 void cxp_error_reset(CXP_Ctx *ctx);
 
 #endif // _CXP_H
